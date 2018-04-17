b3a499b31b215744e7e477e1ec043b25d94eb1a4
Marek Jelen <marek@jelen.biz>
2018-04-17 07:03:30 +0200

    Make Puma configuration configurable

--- application.sh
+++ application.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+export PUMA_THREADS=${PUMA_THREADS:="0:16"}
+
+if [ -n "$PUMA_WORKERS" ]; then 
+  export PUMA_WORKERS="-w $PUMA_WORKERS"
+fi
+
 bundle exec rake db:migrate
-bundle exec puma -w 3 -t 5:5 -e $RAILS_ENV -b tcp://127.0.0.1:3000 --state=/home/ruby/puma.state
+bundle exec puma $PUMA_WORKERS -t $PUMA_THREADS -e $RAILS_ENV -b tcp://127.0.0.1:3000 --state=/home/ruby/puma.state
